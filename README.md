8f6377baf9635b52b8b1bed3f9a8783a3c51ffa9
Joao <joao.demata@correo.unimet.edu.ve>
2021-04-17 23:01:02 -0400

    Primer commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Aplicación de comandos 
+
+Este es el ejercicio del curso 
+
+Recuerden instalar lo spaquetes de node 
+
+```
+npm install
+
+```
---
+++ config/yargs.js
@@ -0,0 +1,32 @@
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripción por la tarea por hacer'
+        }
+    })
+    .command('actualizar', 'Actualiza el esado completado de una tarea', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripción de la tarea por hacer'
+        },
+        completado: {
+            default: true,
+            alias: 'c',
+            desc: 'Marca como completado o pendiente la tarea'
+        }
+
+    })
+    .command('borrar', 'borra una tarea de la lista de tareas', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripción por la tarea por hacer'
+        }
+    })
+    .help()
+    .argv;
+
+module.exports = { argv };
